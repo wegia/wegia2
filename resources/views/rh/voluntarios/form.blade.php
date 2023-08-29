dbbe357c168f9a10b18d3938c43a7fae19d204df
extensao <extensao@extensao.extensao.local>
2023-08-29 17:58:42 -0300

    feat: form de cadastro de voluntarios

--- resources/views/rh/voluntarios/form.blade.php
+++ resources/views/rh/voluntarios/form.blade.php
@@ -11,25 +11,64 @@ Cadastro
 
 @section('breadcrumbs')
 <li class="breadcrumb-item" aria-current="page">
-    <a href="/">
-        <i class="fa fa-home"></i>Home
-    </a>
-</li>
-<li class="breadcrumb-item" aria-current="page">
-    <a href="/rh">
-        <i class="far fa-address-book"></i>Recursos Humanos
-    </a>
-</li>
-<li class="breadcrumb-item" aria-current="page">
-    <a href="/rh/voluntary/adm">
-        <i class="far fa-address-book"></i>Voluntário
-    </a>
-</li>
-<li class="breadcrumb-item active" aria-current="page">
-    <i class="far fa-address-book"></i>Cadastrar
-</li>
+        <a class="text-secondary" href="/">
+            <i class="fa fa-home px-1"></i>Início
+        </a>
+    </li>
+    <li class="breadcrumb-item" aria-current="page">
+        <a class="text-secondary" href="{{route('rhMain')}}">
+            <i class="far fa-address-book px-1"></i>Recursos Humanos
+        </a>
+    </li>
+    <li class="breadcrumb-item" aria-current="page">
+        <a class="text-secondary" href="{{route('rhVoluntariosPainel')}}">
+            <i class="fa-solid fa-address-book px-1"></i>Voluntários
+        </a>
+    </li>
+    <li class="breadcrumb-item active" aria-current="page">
+        <a href="{{route('listaVoluntarios')}}">
+            <i class="fa-solid fa-address-book px-1"></i>Cadastrar Voluntários
+        </a>
+    </li>
 @endsection
 
-@section('scripts-vendors')
+@section('content')
+<div class="content-body">
+    <div class="tab-content bg-dark text-white rounded p-3">
+        <div class="tab-pane fade show active" id="cadastroFuncionario" role="tabpanel" aria-labelledby="cadFunc" tabindex="0">
+            <form action="rh/voluntarios" method="POST"></form>
+                @csrf
+                <!--Descrição-->
+                <div class="row mb-3">
+                    <label class="col-md-3 col-form-label required" for="descricao">
+                        Descrição</label>
+                        <div class="col-md-8">
+                            <input type="text" class="form-control"
+                                id="descricao" name="voluntario.descricao" required>
+                        </div>
+                </div>
+
+                <!--Colaborador-->
+                <div class="row mb-3">
+                    <label class="col-md-3 col-form-label required" for="colaborador">
+                        Colaborador</label>
+                        <div class="col-md-8">
+                            <select name="colaborador.id" id="colaborador">
+                                <option selected disabled>Selecionar</option>
+                                @foreach ($colaboradores as $colaborador)
+                                    <option value="{{$colaborador->id}}">{{$colaborador->pessoa->nome}}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                </div>
+                <!--Disponibilidade Semanal-->
+
+                <!--Disponibilidade Horas-->
+
+        </div>
+    </div>
+</div>
+
+
 
 @endsection
